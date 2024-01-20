c0a0a9ef39a3317c862d9bd5759fabc8021b5d35
Timon <Timon321.86@gmail.com>
2024-01-20 10:55:11 +0300

    Empty commit.

--- src/main/java/it/academy/Runner.java
+++
@@ -1,8 +0,0 @@
-package it.academy;
-
-public class Runner {
-    public static void main(String[] args) {
-        Competition competition = new Competition();
-        competition.start();
-    }
-}
